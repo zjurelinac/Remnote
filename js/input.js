87249cb2023fec4c2135c6e4766b2f8c1a8a020c
Zvonimir Jurelinac <zjurelinac@gmail.com>
2013-11-10 17:31:47 +0100

    Evening commit

--- js/input.js
+++ js/input.js
@@ -1,18 +1,65 @@
 function noteInputer(){
+
 	$( "#input-note-content" ).elastic().keypress( function( e ){
+	
 		var $this = $( this );
 				
 		if( e.keyCode == 9 ){	// tab, prevent focus change
 		
 			e.preventDefault();
 			console.log( $this.caret() );
-			$this.caret( '\t' );
+			$this.caret( '    ' );		/// Enable different tab sizes
 			
 		} else if( e.keyCode == 13 ){	// enter, check for code and lists
+			// if indented, continue indentation
+			e.preventDefault();
+			var ts =  $this.val().slice( 0, $this.caret() );
 			
-			
-			
+			ts = ts.slice( ts.lastIndexOf( '\n' )+1 );
+			console.log( ts );
+						
+			var ma = ts.match( /^[\ \t]* /g );
+			console.log( ma );
+			if( ma != undefined && ma != null )
+				$this.caret( '\n' + ma[ 0 ] );
+			else 
+				$this.caret( '\n' );
 		}
 		
+	});
+}
+
+function toolbarActions(){
+	$toolbar = $( '#input-content-toolbar' );
+	$content = $( '#input-note-content' );
+	$toolbar.find( '#insert-header' ).click( function(){
+		var rng = $content.range();
+		console.log( rng );
+		$content.caret( "\n\n#" ).caret( rng.end + 3 ).caret( '\n\n' ).caret( rng.end + 3 );
+	});
+	
+	$toolbar.find( '#insert-bold' ).click( function(){
+		var rng = $content.range();
+		$content.caret( '*' ).caret( rng.end+1 ).caret( '*' ).caret( rng.end+1 );
+	});
+	
+	$toolbar.find( '#insert-link' ).click( function(){
+	
+	});
+	
+	$toolbar.find( '#insert-image' ).click( function(){
+	
+	});
+	
+	$toolbar.find( '#insert-code' ).click( function(){
+	
+	});
+	
+	$toolbar.find( '#insert-math' ).click( function(){
+	
+	});
+	
+	$toolbar.find( '#show-help' ).click( function(){
+	
 	});
 }
--- js/tooltip.js
+++ js/tooltip.js
@@ -8,13 +8,24 @@
 		}, options );	
 		
 		return this.each( function(){
+			$elem = $( this );
 			var cId = generateRandomInt( 0, 1000000 );
-			this.append( '<div class = "jQtooltip + ' + prop.type + '" + id = "' + cId + '">' + content + '</div' );
-			var $tooltip = this.children( '#' + cId );
-			this.hover( function(){
-				$tooltip.fadeIn();
+			$( document.body ).append( '<div class = "jQtooltip + ' + prop.type + '" + id = "' + cId + '">' + content + '</div>' );
+			
+			var $tooltip = $( '#' + cId );
+			var width = $tooltip.width(), height = $tooltip.height();
+			
+			/*console.log( "[Element:] W = %d, H = %d, X = %d, Y = %d; [Tooltip:] W = %d, H = %d;", $elem.width(), 
+				$elem.height(), $elem.offset().top, $elem.offset().left, width, height );*/
+			$tooltip.css({
+				top : ( $elem.offset().top + $elem.height() + 5 ),
+				left : ( $elem.offset().left + ( width - $elem.width() ) / 2 )
+			});
+			
+			$elem.css( 'cursor', 'pointer' ).hover( function(){
+				$tooltip.show();
 			}, function(){
-				$tooltip.fadeOut();
+				$tooltip.hide();
 			} );
 		});
 		
